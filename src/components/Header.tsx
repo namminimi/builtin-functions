649450206151060e2ef095d904aa0f2b02d88e05
namminimi <tbvhdaos@naver.com>
2023-02-23 10:18:37 +0900

    가짜서버 만든후 header컨포넌트 만듬 스타일드컨포넌트 설치

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const HeaderWrapper = styled.div``
+type HeaderProps = {
+    sitename: string;
+}
+
+const Header = ({sitename}: HeaderProps) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Header;
