eb88883adea1b0f7de816bfd9ffcf8e1917adfcc
namminimi <tbvhdaos@naver.com>
2023-02-23 10:24:02 +0900

    Header완성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import styled from 'styled-components'
 
-const HeaderWrapper = styled.div``
+const HeaderWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 30px;
+`;
+
+const HeaderUl = styled.ul`
+    dispaly: flex;
+    align-items: center;
+    li{
+        list-style:none;
+        padding: 0 20px;
+    }
+`
 type HeaderProps = {
     sitename: string;
 }
 
 const Header = ({sitename}: HeaderProps) => {
     return (
-        <div>
-            
-        </div>
+        <HeaderWrapper>
+            <h1>{sitename}</h1>
+            <HeaderUl>
+                <li>과목등록</li>
+                <li>내장함수등록</li>
+                <li>함수리스트</li>
+            </HeaderUl>
+        </HeaderWrapper>
     );
 };
 
